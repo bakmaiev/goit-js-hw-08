ea3b962fe721828fc45c4f5a1a2df28599ee7c3c
bakmaiev <thisyoungrhyme@gmail.com>
2023-03-09 18:18:37 +0200

    task-01 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+galleryEl.insertAdjacentHTML('beforeend', createItemGalleryMarkup(galleryItems));
 
-console.log(galleryItems);
+function createItemGalleryMarkup() {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}" 
+                alt="${description}" />
+        </a>`;
+    }).join('');
+};
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 });;
